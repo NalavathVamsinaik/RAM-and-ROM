6ac3e6db7315719c503a8baebffffbe48b09a40f
VAMSI NAIK NALAVATH <91309755+NalavathVamsinaik@users.noreply.github.com>
2023-04-06 08:14:46 +0530

    Create testbench.v

---
+++ ROM memory/testbench.v
@@ -0,0 +1,39 @@
+module single_port_ROM_tb();
+
+    reg clk, enb;
+    reg [2:0]addr;
+    wire [7:0]out;
+    
+    rom uut (.clk(clk), .enb(enb), .addr(addr), .out(out));
+    
+    always #5 clk=~clk;
+    initial begin
+        clk=0; enb=0; 
+        #100;
+        
+        enb=1;
+        addr=3;
+        #10;
+        
+        addr=2;
+        #10;
+        
+        addr=7;
+        #10;
+        
+        addr=0;
+        #10;
+        
+        enb=0;
+        #100;
+        
+        enb=1;
+        addr=6;
+        #10;
+        
+        addr=2;
+        #10;
+        
+        #1000; $finish;
+     end
+endmodule
